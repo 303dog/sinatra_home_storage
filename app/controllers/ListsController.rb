4aecd0a0ef6719176a03bae3b9674034b99b3f66
Paul_Beckwith <303dog@gmail.com>
2020-07-31 20:10:26 -0600

    post/new action

--- app/controllers/ListsController.rb
+++ app/controllers/ListsController.rb
@@ -6,9 +6,16 @@ class ListsController < ApplicationController
     end                                     # items that correspond to the current user. 
                                             # Then directs to the erb where it is told to render that "view".
     
-    get '/lists/new' do     #new action/ view for form to create new items for the user
-        erb :'items/new'
+    get '/lists/new' do                     #new action/ view for form to create new items for the user
+        erb :'lists/new'
     end
 
-
+    post '/lists' do  
+        item = current_user.items.build(params)
+        if item.save
+          redirect "items/#{item.id}"
+        else
+            erb :'items.new'
+        end                   #create action used by new.erb
+    end
 end
